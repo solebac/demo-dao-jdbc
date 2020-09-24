6a4b6fa8fe16a9ee90555420c6653d4089d53ef6
solebac <solebac@hotmail.com>
2020-09-24 13:08:21 -0300

    Deparment Alter

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1, "Logico");
+		System.out.println(obj);
+
+	}
+
+}
