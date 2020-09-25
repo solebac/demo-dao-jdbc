c481349fb8228723f4ef85d45149202018e63f09
solebac <solebac@hotmail.com>
2020-09-25 01:11:41 -0300

    SellerDaoJdbc Factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
@@ -11,6 +13,10 @@ public class Program {
 		Department obj = new Department(1, "Logico");
 		Seller seller = new Seller(21, "Bob", "solebac@solebac", new Date(), 1000.0, obj);
 		System.out.println(obj);System.out.println(seller);
+		
+		//SellerDao objSellerModoErrado = new Selle
+		//Eu implemento a fabrica de Dao's, oculto do programa principal
+		SellerDao objSellerDao = DaoFactory.createSellerDao();//Implementou as 5 intefaces...
 
 	}
 
